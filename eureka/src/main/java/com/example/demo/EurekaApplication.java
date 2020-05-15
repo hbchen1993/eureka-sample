d204c0726018c5541bf41358cf54478e58148939
1399414266@qq.com <1399414266@qq.com>
2020-05-15 11:03:38 +0800

    冲突测试1

--- eureka/src/main/java/com/example/demo/EurekaApplication.java
+++ eureka/src/main/java/com/example/demo/EurekaApplication.java
@@ -13,6 +13,8 @@ public class EurekaApplication {
         System.out.println("主分支修改");
         System.out.println("新分支又修改了");
         System.out.println("新分支又又修改了");
+
+        System.out.println("新分支又又修改了1");
         SpringApplication.run(EurekaApplication.class, args);
     }
 
