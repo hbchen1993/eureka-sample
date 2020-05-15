c94d1271a928f63528a8dfd7dc579f90f1d131ca
1399414266@qq.com <1399414266@qq.com>
2020-05-15 10:31:16 +0800

    新的分支

--- eureka/src/main/java/com/example/demo/EurekaApplication.java
+++ eureka/src/main/java/com/example/demo/EurekaApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaApplication {
 
     public static void main(String[] args) {
+        System.out.println("新的分支");
         SpringApplication.run(EurekaApplication.class, args);
     }
 
